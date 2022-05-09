56d0838ef68545cf60088c29264b32e5225f261a
Propietario <Propietario@10.6.6.80>
2022-05-09 20:39:54 +0200

    Este sería el primer commit que realizo.

---
+++ persona/src/persona/persona.java
@@ -0,0 +1,14 @@
+package persona;
+
+public class persona {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private static void displayGreeting() {
+		System.out.println("Hola!");
+	} 
+
+}
